972fb9f648862d732172ec4d5081e530b3636bba
Steven <steven.s.r.garcia@gmail.com>
2015-11-09 15:23:51 -0500

    Added code to pull linkedin company info

--- routes/index.js
+++ routes/index.js
@@ -8,38 +8,26 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
-router.get('/demo', function(req, res, next) {
-  res.render('demo');
-});
- 
-router.get('/api/smartreads', function(req, res){
+/*============================== LINKEDIN =====================================================*/
+/* Get /api/linkedin.. Events Api data and send it out  */
+router.get('/linkedin', function(req, res, next) {
 	var options = {
-		uri: 'https://www.linkedin.com/company/accordant-media---what-we-are-reading',
-		timeout: 2000,
-		followAllRedirects: true
+		url: 'https://api.linkedin.com/v1/companies/6628793/updates?format=json',
+		headers: {
+			'User-Agent': 'MyApp(steven@basecamp.com)'
+		},
+		auth:{
+			'bearer': 'AQUL4iTcKGlEd9pRLnX3W0O4PoBfhkrIFwwr0aDw0RAoq75PXqQBa4rmNd0E7UeR13VBIvunekW7Qh72hPoZk9hC8l_InFv3EClfTlWwdpkujeV238ICuoS-km9Sxf3ggMn1-gh4Nifl295A79RaKTfciL7MK5cGr8BqSJ2Iw-dk2SlnY2M'
+		}
 	};
-	request(options, function (err, response, body) {
 
-		request(response.request.uri.href, function(err, response, body){
-			console.log('URL' + response.request.uri.href);
-			console.log(body);
-			$ = cheerio.load(body);
-			var header = $('ul#my-feed-post').html();
-			var newArray = [];
-			for(var i = 0; i < 4; i++){
-				newArray.push({
-					head  : $('ul#my-feed-post').children().eq(i).children().children().eq(1).children().eq(1).html(),
-					title : $('ul#my-feed-post').children().eq(i).children().children().eq(1).children().eq(2).children().eq(1).children().eq(0).html(),
-					text  : $('ul#my-feed-post').children().eq(i).children().children().eq(1).children().eq(2).children().eq(1).children().eq(2).html()
-				});
-			}
-			//console.log(header);
-			res.json(body);
+	request(options, function(error, message, linkedinFeed){
+		console.log(linkedinFeed);
+		res.json(linkedinFeed);
+	}); // Closes request()
+}); // Closes router.get
 
 
-		});
-	});
-});
 
 router.get('/api/google', function(req, res){
 	request('http://google.com', function (err, response, body) {
